7223779e286a4f903ec0d036e6d9f92e587efae5
Thomas Ley <thomas-ley@outlook.de>
2025-03-21 14:10:23 +0100

    Empty homepage with data privacy and welcome message

---
+++ src/Softwarekueche.Web/Pages/Meta/Datenschutz.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Softwarekueche.Web.Pages.Meta
+{
+    public class DatenschutzModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
