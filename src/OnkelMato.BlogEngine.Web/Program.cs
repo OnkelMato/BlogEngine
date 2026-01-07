d9ad5667cf27a8ebdfb613f84ac23a3c67fe0ab8
Thomas Ley <thomas-ley@outlook.de>
2026-01-07 18:28:14 +0100

    reenable data protecten keys persistence, blogadmin returns a list of blog for later purpose.

--- src/OnkelMato.BlogEngine.Web/Program.cs
+++ src/OnkelMato.BlogEngine.Web/Program.cs
@@ -1,6 +1,14 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Logging;
 
 namespace OnkelMato.BlogEngine.Web;
 
@@ -10,32 +18,37 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        //string dataProtectionKeysPath;
-        //try
-        //{
-        //     dataProtectionKeysPath = builder.Configuration.GetValue<string>("SystemConfig:DataProtectionKeysPath") ?? string.Empty; ;
-        //}
-        //catch (Exception)
-        //{
-        //    dataProtectionKeysPath = string.Empty;
-        //}
+        // set data protection key persistence
+        try
+        {
+            var dataProtectionKeysPath = builder.Configuration.GetValue<string>("SystemConfig:DataProtectionKeysPath") ?? string.Empty;
+            Directory.CreateDirectory(dataProtectionKeysPath);
+            if (!string.IsNullOrWhiteSpace(dataProtectionKeysPath) && Directory.Exists(dataProtectionKeysPath))
+                builder.Services.AddDataProtection()
+                    .UseCryptographicAlgorithms(
+                        new AuthenticatedEncryptorConfiguration
+                        {
+                            EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+                            ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+                        })
+                    .PersistKeysToFileSystem(new DirectoryInfo(dataProtectionKeysPath));
+        }
+        catch (Exception ex)
+        {
+            Trace.TraceError("Cannot set data protection key persistence. Details: " + ex.Message);
+        }
 
+        // add all blog engine infrastructure services
         builder.AddBlogEngine();
+
+        // add routing for blog posts to be more SEO friendly
         builder.Services.AddRazorPages(options =>
         {
             options.Conventions.AddPageRoute("/Posts", "/Post/{titleStub}/{id}");
         });
         builder.Services.AddBlogSeoTags();
 
-        //if (!string.IsNullOrWhiteSpace(dataProtectionKeysPath) && Directory.Exists(dataProtectionKeysPath))
-        //    builder.Services.AddDataProtection()
-        //        .UseCryptographicAlgorithms(
-        //            new AuthenticatedEncryptorConfiguration
-        //            {
-        //                EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
-        //                ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
-        //            })
-        //        .PersistKeysToFileSystem(new DirectoryInfo(dataProtectionKeysPath));
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
